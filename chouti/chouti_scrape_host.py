8bd2a33c67ca1ca14340f9ea00154df9ea033979
kubiMiner <laolongkou@gmail.com>
2017-03-17 07:46:58 +0800

    Update chouti_scrape_host.py

--- chouti/chouti_scrape_host.py
+++ chouti/chouti_scrape_host.py
@@ -1,15 +1,14 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[ ]:
 
 import json, re
 from configparser import ConfigParser
 import urllib.request
 
 
-# In[4]:
-
+# In[ ]:
 
 # reading configration from config.ini file 
  
@@ -35,8 +34,7 @@ def read_db_config(filename='mysql_config.ini', section='mysql'):
     return db
 
 
-# In[35]:
-
+# In[ ]:
 
 # establishing mysql conection
 
@@ -64,8 +62,7 @@ def establish_mysql_connect(config_file, echo=True):
         return False
 
 
-# In[9]:
-
+# In[ ]:
 
 # getting rebang json by urllib.request method
 
@@ -85,8 +82,7 @@ def get_rebang_json(page_num):
     return data
 
 
-# In[33]:
-
+# In[ ]:
 
 # return the list of dict from json to mysql REPLACE SQL query
 
@@ -102,30 +98,4 @@ def dict_to_query(dict_list, table_name):
         query += ["REPLACE INTO `{table}` ({columns}) VALUES ({values});"
                   .format(table=table_name, columns=fld_name, values=value_str)]
     return query
-
-
-# In[34]:
-
-dict_list = get_rebang_json(1)
-dict_to_query(dict_list, 'bang')
-
-
-# In[ ]:
-
-import pandas as pd
-df = pd.DataFrame(data)
-print(df[['id', 'url']])
-
-def get_redirected_url(url):
-    opener = urllib.request.build_opener(urllib.request.HTTPRedirectHandler)
-    request = opener.open(url)
-    return request.url
-	
-url = 'http://dig.chouti.com/mobile/link/10797361'
-url = get_redirected_url(url)
-
-
-from urllib.parse import urlparse
-parsed = urlparse(url)
-print(parsed)
 
