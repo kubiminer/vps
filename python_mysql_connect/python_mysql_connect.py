01f175121ddc71cded9caa585ca6f9405ed4bbb3
kubiMiner <laolongkou@gmail.com>
2017-03-16 21:55:04 +0800

    Create python_mysql_connect.py

---
+++ python_mysql_connect/python_mysql_connect.py
@@ -0,0 +1,30 @@
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+
+config_file = '/home/karibu/conf/mysql_config.ini'
+ 
+ 
+def connect():
+    """ Connect to MySQL database """
+ 
+    db_config = read_db_config(config_file, 'mysql')
+ 
+    try:
+        print('Connecting to MySQL database...')
+        conn = MySQLConnection(**db_config)
+ 
+        if conn.is_connected():
+            print('connection established.')
+        else:
+            print('connection failed.')
+ 
+    except Error as error:
+        print(error)
+ 
+    finally:
+        conn.close()
+        print('Connection closed.')
+ 
+ 
+if __name__ == '__main__':
+    connect()
