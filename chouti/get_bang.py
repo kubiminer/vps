dfb03811901bc7f6e79ea864f7a3878ef5affc8e
kubiMiner <laolongkou@gmail.com>
2017-03-17 16:28:41 +0800

    Update get_bang.py

--- chouti/get_bang.py
+++ chouti/get_bang.py
@@ -1,32 +1,37 @@
 # constant defination
 mysql_config_file = '/home/karibu/.conf/mysql_config.ini'
-log_file = '/var/www/html/log.html'
+log_file = '/var/www/html/log.html' #/var/www/html/
 
-# 
-def list_to_string(l):
-    string = ''
-    for i in l:
-        string += str(i) + '\n'
 
 from chouti_scrape_host import establish_mysql_connect, get_rebang_json, dict_to_query
+import time
+
+class Log():
+    def __init__(self, filename='log.txt', mode='w'):
+        self.handle = open(filename, mode, encoding='utf-8')
+        self.handle.write(self.now() + filename + 'opened in ' + mode + ' mode' + '\n')
+    def write(self, text):
+        self.handle.write(self.now() + str(text) + '\n')
+    def close(self):
+        self.handle.write(self.now() + "log closed" + '\n')
+        self.handle.close()
+    def now(self):
+        return time.strftime('%Y-%m-%D %H:%M.%S >>> ', time.localtime())        
 
 # main Program
 if __name__ == '__main__':
 
     #conn = establish_mysql_connect(mysql_config_file, False)
 
-    log_file = open(log_file, 'w', encoding="utf-8") 
+    log = Log() 
 
     dicts = get_rebang_json(10)
-    print(len(dicts)
-          
-    #log_file.write(list_to_string(dicts))
-
-    query = dict_to_query(dicts, 'chouti'))
+    print(len(dicts))
+    log.write(dicts
+             )
+    query = dict_to_query(dicts, 'chouti')
     print(len(query))
-    #log_file.write('<html>/n') 
-    #log_file.write(list_to_string(query))
-    #log_file.write('</html>') 
+    log.write(query)
     
-    log_file.close()
-    conn.close()
+    log.close()
+    #conn.close()
