e02aecf19e9b55ef6823e0ecd8b348032b15a9ec
kubiMiner <laolongkou@gmail.com>
2017-03-17 12:33:34 +0800

    Update get_bang.py

--- chouti/get_bang.py
+++ chouti/get_bang.py
@@ -5,9 +5,11 @@ from chouti_scrape_host import establish_mysql_connect
 
 # main Program
 if __name__ == '__main__':
-    conn = establish_mysql_connect(mysql_config_file, False)
-	dicts = get_rebang_json(1)
-    print(dicts)
 	
-	query = dict_to_query(dicts, table_name)
-	print(query, 'chouti')
+    conn = establish_mysql_connect(mysql_config_file, False)
+
+    dicts = get_rebang_json(1)
+    print(dicts)
+
+    query = dict_to_query(dicts, table_name)
+    print(query, 'chouti')
